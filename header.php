1cfa98d849d50be596f7a8188425e4d0b879d3fb
Micael Hernandes <micaelmacedo100@outlook.com>
2020-12-27 09:36:21 -0300

    Particionando codigo

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+session_start();//iniciando sessão
+?>
+
+<!DOCTYPE html>
+<html>
+    
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Login - PHP + MySQL - Canal TI</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
